2a7aabd96914358c558e782472684ac402deb630
SSL <38443952+song88180@users.noreply.github.com>
2018-06-26 22:58:13 +0800

    Create filterfas.sh

---
+++ filterfas.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#(Hsar Hdem Htel)(Hhsa HeraRef Hhim)
+while read line
+do
+  arr=(${line// / })
+  sed -n '/>Hsar/{p;n;p};/>Hdem/{p;n;p};/Htel/{p;n;p};/>Hhsa/{p;n;p};/>HeraRef/{p;n;p};/>HeraDisco/{p;n;p};/>Hhim/{p;n;p}' sepgenes/${arr[3]}.fas > sepgenes/${arr[3]}_filtered.fas
+  num=`grep ">Hsar\|>Hdem\|>Htel\|>Hhsa\|>HeraRef\|>HeraDisco\|>Hhim" sepgenes/${arr[3]}_filtered.fas | wc -l`
+  if [ "$num" -ne 7 ]; then
+    echo "${arr[3]}:$num:removed"
+  fi
+done < Herato_chr2_filtered_290.bed
